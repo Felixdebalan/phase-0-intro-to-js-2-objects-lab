c2c691a001c9aa232c1a9098a9f54d19b574905e
Felix De Balanzo <felixdebalan@gmail.com>
2022-10-21 23:12:06 -0400

    commit tree b842fa812a5526a2b8b8c99b01cc543b0a98927c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+const employee = {
+    name: "Felix",
+    streetAddress:"340 East", 
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}
+    
+
+}
